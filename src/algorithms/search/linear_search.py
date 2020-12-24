31565e55e681b8a9ba30ee1669b9b6cd4826c03a
Ameer Taweel <ameertaweel2002@gmail.com>
2020-12-24 22:43:53 +0200

    docs: Update time and space complexity for linear search

--- src/algorithms/search/linear_search.py
+++ src/algorithms/search/linear_search.py
@@ -1,8 +1,12 @@
 """
 Linear Search Implementations
 
-Time Complexity: O(n) "Linear"
-Space Complexity: O(1) "Constant"
+Time Complexity:
+	Worst: O(n) "Linear"
+	Average: O(n/2)
+	Best: O(1)
+Space Complexity:
+	Worst: O(1) "Constant"
 """
 
 ELEMENT_NOT_FOUND = -1
