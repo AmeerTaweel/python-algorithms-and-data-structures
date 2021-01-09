527d06add7ad6415e816f9b17c6e8fe41a5c1a7a
Ameer Taweel <ameertaweel2002@gmail.com>
2021-01-09 10:46:59 +0200

    Format with black and isort

--- src/algorithms/search/binary_search.py
+++ src/algorithms/search/binary_search.py
@@ -13,24 +13,25 @@ import math
 
 ELEMENT_NOT_FOUND = -1
 
+
 def binary_search(items, search_item):
-	"""
-	Implementation of the binary search algorithm.
+    """
+    Implementation of the binary search algorithm.
 
-	Return the index of the search_item if it is in a sorted list.
-	The list must be sorted for this to work
-	Return -1 otherwise.
-	"""
-	start = 0
-	end = len(items) - 1
+    Return the index of the search_item if it is in a sorted list.
+    The list must be sorted for this to work
+    Return -1 otherwise.
+    """
+    start = 0
+    end = len(items) - 1
 
-	while start <= end:
-		mid = start + math.floor((end - start) / 2)
-		if items[mid] == search_item:
-			return mid
-		if items[mid] > search_item:
-			end = mid - 1
-		else:
-			start = mid + 1
+    while start <= end:
+        mid = start + math.floor((end - start) / 2)
+        if items[mid] == search_item:
+            return mid
+        if items[mid] > search_item:
+            end = mid - 1
+        else:
+            start = mid + 1
 
-	return ELEMENT_NOT_FOUND
+    return ELEMENT_NOT_FOUND
--- src/algorithms/search/linear_search.py
+++ src/algorithms/search/linear_search.py
@@ -11,16 +11,17 @@ Space Complexity:
 
 ELEMENT_NOT_FOUND = -1
 
-def linear_search(items, search_item, are_equal = lambda x, y: x == y):
-	"""
-	Implementation of the linear search algorithm.
 
-	Accepts an optional custom comparing function.
+def linear_search(items, search_item, are_equal=lambda x, y: x == y):
+    """
+    Implementation of the linear search algorithm.
 
-	Return the index of the search_item if it is in the list.
-	Return -1 otherwise.
-	"""
-	for i, item in enumerate(items):
-		if are_equal(item, search_item):
-			return i
-	return ELEMENT_NOT_FOUND
+    Accepts an optional custom comparing function.
+
+    Return the index of the search_item if it is in the list.
+    Return -1 otherwise.
+    """
+    for i, item in enumerate(items):
+        if are_equal(item, search_item):
+            return i
+    return ELEMENT_NOT_FOUND
