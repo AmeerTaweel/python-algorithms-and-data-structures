b1ee221966003443eaa73d1cf449b5b0f9ef6037
Ameer Taweel <ameertaweel2002@gmail.com>
2021-01-16 22:41:52 +0200

    Add custom_linear_search back

--- src/algorithms/search/linear_search.py
+++ src/algorithms/search/linear_search.py
@@ -33,15 +33,21 @@ def linear_search(search_list, target):
     return ELEMENT_NOT_FOUND
 
 
-def linear_search(items, search_item, are_equal=lambda x, y: x == y):
+def custom_linear_search(items, search_item, are_equal=lambda x, y: x == y):
     """
     Implementation of the linear search algorithm.
-
     Accepts an optional custom comparing function.
 
-    Return the index of the search_item if it is in the list.
-    Return -1 otherwise.
+    Input:
+        search_list: List of items (Usually numbers)
+        target: The item to find
+        are_equal: Custom function to determine which items are equal
+
+    Output:
+        Return the index of the target if it is in the list.
+        Return -1 otherwise.
     """
+
     for i, item in enumerate(items):
         if are_equal(item, search_item):
             return i
